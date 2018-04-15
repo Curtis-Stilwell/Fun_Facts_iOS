9cae69762b473c0507dc847077dce15858f89b2e
Curtis-Stilwell <curtisstilwell1@curtiss-macbook-pro.local>
2018-04-14 19:32:16 -0500

    Initial Commit

---
+++ FunFacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunFacts
+//
+//  Created by Curtis Stilwell on 4/14/18.
+//  Copyright © 2018 Curtis Stilwell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
