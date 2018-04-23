53832d36804d2fa15370604c4e0211fd2f8c5953
Curtis-Stilwell <curtisstilwell1@Curtiss-MacBook-Pro.local>
2018-04-22 20:58:16 -0500

    first commit

---
+++ FunFacts/BackgroundColorProvider.swift
@@ -0,0 +1,9 @@
+//
+//  BackgroundColorProvider.swift
+//  FunFacts
+//
+//  Created by Curtis Stilwell on 4/22/18.
+//  Copyright © 2018 Curtis Stilwell. All rights reserved.
+//
+
+import Foundation
